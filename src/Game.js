836aee8d70af5264c6eb5bb0eb84b5738bf21c25
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-29 17:56:30 -0600

    Create and pass all tests in Game-test.js

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,19 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Round = require('../src/Round');
+
+//Global Variables
+var deck
+var card
+var round
+
 
 class Game {
-  constructor() {}
+  constructor() {
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +23,23 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+  start(cardInfo) {
+    cardInfo.map((elem) => {
+    card = new Card(elem.id, elem.question, elem.answers, elem.correctAnswer)
+    deck = new Deck(cardInfo)
+    round = new Round(deck)
+    // this.printMessage(deck, round)
+    // this.printQuestion(round)
+    }) 
+  }
+ checkDeckForCard(cardInfo) {
+  return deck.cards === cardInfo
+ }
+ checkForRound() {
+  if (round.deck === deck) {
+    return round
+  }
+ }
 }
 
 module.exports = Game;
