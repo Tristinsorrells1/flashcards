071ea958da36e0fb9999c371cb075ea188e6d714
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-28 20:55:37 -0600

    Test all the Turn methods

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,28 @@ class Turn {
     this.answer = answer;
     this.currentCard = currentCard
   }
+
+  returnGuess() {
+    return this.answer
+  }
+
+  returnCard() {
+    return this.currentCard
+  }
+
+  evaluateGuess() {
+   if (this.answer === this.currentCard.correctAnswer) {
+    return true
+   }
+   return false
+  }
+
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+        return 'correct!'
+    }
+    return 'incorrect!'
+  }
 }
 
 
