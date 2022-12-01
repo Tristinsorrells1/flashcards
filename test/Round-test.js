28592140502ec4529d6ad5f9139d3e00c6b34c97
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-12-01 15:13:50 -0600

    Refactor tests for startOver method in Round.js

--- test/Round-test.js
+++ test/Round-test.js
@@ -8,11 +8,11 @@ const Turn = require("../src/Turn");
 const Deck = require("../src/Deck");
 
 describe("Round", function () {
-	let answers;
 	let cards;
+	let answers;
 	let deck;
-	let turn;
 	let round;
+	let turn;
 	beforeEach("test setup", function () {
 		cards = [prototypeData[0], prototypeData[1], prototypeData[2]];
 		answers = [
@@ -22,33 +22,26 @@ describe("Round", function () {
 		];
 		deck = new Deck(cards);
 		round = new Round(deck);
+		turn = new Turn(answers[0], cards[0]);
 	});
+
+	it("should be a function", function () {
+		expect(Round).to.be.a("function");
+	});
+
 	it("should be an instance of Round", function () {
 		expect(round).to.be.an.instanceof(Round);
 	});
-	it("should have a deck", function () {
+
+	it("should store a Deck", function () {
 		expect(round.deck).to.equal(deck);
 	});
-	it("should create a new Turn instance", function () {
-		round.takeTurn(answers[0]);
-		expect(round.turn).to.be.an.instanceof(Turn);
+
+	it("should set current Card as the first Card in the Deck at the start of the Round", function () {
+		expect(deck.cards[0]).to.equal(round.currentCard);
 	});
-	it("should store the current turn", function () {
-		round.takeTurn(answers[0]);
-		expect(round.turn).to.be.an.instanceof(Turn);
-	});
-	it("should start every round with 0 turns", function () {
-		expect(round.turns).to.equal(0);
-	});
-	it("should update the turns count by one each round", function () {
-		round.takeTurn(answers[0]);
-		expect(round.turns).to.equal(1);
-		round.takeTurn(answers[1]);
-		expect(round.turns).to.equal(2);
-		round.takeTurn("wrong guess");
-		expect(round.turns).to.equal(3);
-	});
-	it("should update current card to the next card", function () {
+
+	it("should update current Card to the next Card", function () {
 		expect(round.currentCard).to.equal(prototypeData[0]);
 
 		round.takeTurn(answers[1]);
@@ -57,25 +50,33 @@ describe("Round", function () {
 		round.takeTurn("wrong guess");
 		expect(round.currentCard).to.equal(prototypeData[2]);
 	});
-	it("should return the current card being played", function () {
-		expect(round.returnCurrentCard()).to.equal(cards[0]);
+	
+	it("should start every round with 0 turns", function () {
+		expect(round.turns).to.equal(0);
+	});
 
+	it("should create a new Turn each round", function () {
+		round.takeTurn(answers[0]);
+		expect(round.turn).to.be.an.instanceof(Turn);
+	});
+
+	it("should store the Turn", function () {
+		round.takeTurn(answers[0]);
+		expect(round.turn).to.deep.equal(turn);
+	});
+
+	it("should update the turns count by one each round", function () {
+		round.takeTurn(answers[0]);
+		expect(round.turns).to.equal(1);
 		round.takeTurn(answers[1]);
-		expect(round.returnCurrentCard()).to.equal(cards[1]);
+		expect(round.turns).to.equal(2);
+		round.takeTurn("wrong guess");
+		expect(round.turns).to.equal(3);
+	});
 
-		round.takeTurn("incorrect answer");
-		expect(round.returnCurrentCard()).to.equal(cards[2]);
-	});
-	it("should set currentCard as the first Card in the Deck at the start of the Round", function () {
-		expect(deck.cards[0]).to.equal(round.currentCard);
-	});
-	it("should return 'correct!' if the answer matches the card", function () {
-		expect(round.takeTurn(answers[0])).to.equal("correct!");
-	});
-	it("should return 'incorrect!' if the answer matches the card", function () {
-		expect(round.takeTurn("wrong guess")).to.equal("incorrect!");
-	});
 	it("should store incorrect guesses", function () {
+		expect(round.incorrectGuesses).to.deep.equal([]);
+		
 		round.takeTurn(answers[0]);
 		expect(round.incorrectGuesses).to.deep.equal([]);
 
@@ -85,16 +86,66 @@ describe("Round", function () {
 		round.takeTurn("wrong");
 		expect(round.incorrectGuesses).to.deep.equal([2, 3]);
 	});
+
+	it("should return the current Card being played", function () {
+		expect(round.returnCurrentCard()).to.equal(cards[0]);
+
+		round.takeTurn(answers[1]);
+		expect(round.returnCurrentCard()).to.equal(cards[1]);
+
+		round.takeTurn("incorrect answer");
+		expect(round.returnCurrentCard()).to.equal(cards[2]);
+	});
+
+	it("should return 'correct!' if the answer matches the Card", function () {
+		expect(round.takeTurn(answers[0])).to.equal("correct!");
+	});
+
+	it("should return 'incorrect!' if the answer matches the Card", function () {
+		expect(round.takeTurn("wrong guess")).to.equal("incorrect!");
+	});
+
 	it("should return the percentage of correct guesses", function () {
 		round.takeTurn(answers[0]);
 		round.takeTurn("wrong");
-		expect(round.calculatePercentCorrect()).to.equal(50.00);
+		expect(round.calculatePercentCorrect()).to.equal(50.0);
 	});
+
 	it("should print the percentage of correct questions", function () {
+		round.takeTurn(answers[0]);
+		expect(round.endRound()).to.equal(
+			`** Round over! ** You answered 100.00% of the questions correctly!`
+		);
+	});
+
+	it("should start a new Round if they scored less than 90%", function () {
 		round.takeTurn(answers[0]);
 		round.takeTurn("wrong");
 		expect(round.endRound()).to.equal(
-			`** Round over! ** You answered 50.00% of the questions correctly!`
+			`** New round! Repeat flashcards until you score a 90.00% or better **`
 		);
 	});
+
+	it("should reset the number of turns in the new Round", function () {
+		round.takeTurn(answers[0]);
+		round.takeTurn("wrong");
+		round.endRound();
+		expect(round.turns).to.equal(0)
+	});
+
+	it("should reset the number of incorrect guesses in the new Round", function () {
+		round.takeTurn(answers[0]);
+		round.takeTurn("wrong");
+		round.endRound();
+		expect(round.incorrectGuesses).to.deep.equal([]);
+	});
+
+	it("should reset current Card to the first Card in the Deck in the new Round", function () {
+		round.takeTurn(answers[0]);
+		round.takeTurn("wrong");
+		round.endRound();
+		expect(round.currentCard).to.deep.equal(cards[0]);
+	});
+
+
 });
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -15,32 +15,45 @@ describe("Turn", function () {
 		turn = new Turn(correctGuess, currentCard);
 		turn2 = new Turn("wrong", currentCard);
 	});
+
+	it("should be a function", function () {
+		expect(Turn).to.be.a("function");
+	});
+
 	it("should be an instance of Turn", function () {
 		expect(turn).to.be.an.instanceof(Turn);
 	});
+
 	it("should store a user's answer", function () {
 		expect(turn.answer).to.equal(correctGuess);
 		expect(turn2.answer).to.equal("wrong");
 	});
-	it("should store the current flashcard", function () {
-		expect(turn.currentCard).to.equal(currentCard);
-	});
-	it("should return the user's guess", function () {
+
+	it("should return the user's answer", function () {
 		expect(turn.returnGuess()).to.equal(correctGuess);
 		expect(turn2.returnGuess()).to.equal("wrong");
 	});
+
+	it("should store the current flashcard", function () {
+		expect(turn.currentCard).to.equal(currentCard);
+	});
+
 	it("should return the current flashcard", function () {
 		expect(turn.returnCard()).to.equal(currentCard);
 	});
+
 	it("should return true if the user's guess matches the answer on the card", function () {
 		expect(turn.evaluateGuess()).to.equal(true);
 	});
+
 	it("should return false if the user's guess does not match the answer on the card", function () {
 		expect(turn2.evaluateGuess()).to.equal(false);
 	});
+
 	it("should return 'correct!' when the user's guess is correct", function () {
 		expect(turn.giveFeedback()).to.equal("correct!");
 	});
+
 	it("should return 'incorrect!' when the user's guess is incorrect", function () {
 		expect(turn2.giveFeedback()).to.equal("incorrect!");
 	});
